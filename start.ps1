f1599428225a3d434fc1e1aa0325b083def3f221
Dharun <105167938+Dharun07-17@users.noreply.github.com>
2026-04-16 17:47:13 +0530

    Delete start.ps1

--- start.ps1
+++
@@ -1,5 +0,0 @@
-Start-Process powershell -ArgumentList "-NoExit", "-Command", `
-"cd backend; npm install; npm run dev"
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", `
-"cd frontend; npm install; npm run dev"
